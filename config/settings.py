769f077fe127b865a2c91e020fe49907bc3a999b
Anitha <you@example.com>
2026-03-05 10:51:30 +0530

    Prepare project for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,7 @@ STATICFILES_DIRS=[
     BASE_DIR / "static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
